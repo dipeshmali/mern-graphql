d2a0450f16522ef6c7b2d31387510edd72fe846e
Dipesh Mali <dipeshmali44@gmail.com>
2019-11-23 14:50:32 +0530

    app.js update

--- app.js
+++ app.js
@@ -43,8 +43,22 @@ app.use('/graphql',
 );
 app.use('/', express.static(path.join(__dirname, 'react-frontend/build')))
 
-// app.get('/', )
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    console.log('error', err.message);
+    next();
+    // res.render('error');
+});
+
 
 app.listen(8080, () => {
-    console.log('App runing on port : 8080');
+    console.log('server is running on port 8080');
 });
+
+
